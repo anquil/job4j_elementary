bf2bbe3e1ea9ed957634ef8567fd7a13d7b165ca
Anquil <7529054@mail.ru>
2021-03-29 18:21:46 +0300

    3.2 Фиксация изменений на github [#478610]

---
+++ src/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alex Karchevskiy");
+        System.out.println("19.03.1977");
+
+    }
+}
---
+++ src/job4j/ru/job4j/Multiple.java
@@ -0,0 +1,7 @@
+package job4j.ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+
+    }
+}

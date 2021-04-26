62d5539474f186df5666520faecd7298567a137a
Anquil <7529054@mail.ru>
2021-04-27 00:12:44 +0300

    3.2фиксация изменений на github [#478586]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -16,4 +16,5 @@ public class Fit {
         System.out.println("Man 187 is " + man);
         System.out.println("Woman 187 is " + woman);
     }
+
 }
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,7 +2,10 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, double k) {
-        return ((p / (2*(k + 1)))*k) * (p / (2*(k + 1)));
+        // return ((p / (2*(k + 1)))*k) * (p / (2*(k + 1)));
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        return l * h;
     }
     public static void main(String[] args) {
         double result1 = SqArea.square(6,2);

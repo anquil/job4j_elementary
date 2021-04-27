6cb7318e2f70a5c88ffea4900733139677d93810
Anquil <7529054@mail.ru>
2021-04-27 16:05:58 +0300

    9/1 фиксация изменений на github [#478593]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I lake Java" ;
+        String idea = "I lake Java";
         System.out.println(idea);
         idea = idea + " But I am newbie ";
         int year = 2021;

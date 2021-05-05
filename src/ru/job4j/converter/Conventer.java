214300fec72b2f55f3240ec2e5cd91a29c9af75d
Anquil <7529054@mail.ru>
2021-05-05 15:04:51 +0300

    9/1 фиксация изменений на github [#478615]

--- src/ru/job4j/converter/Conventer.java
+++ src/ru/job4j/converter/Conventer.java
@@ -1,19 +1,21 @@
 package ru.job4j.converter;
 
 public class Conventer {
-    public static float rubleToEuro(float value){
+    public static float rubleToEuro(float value) {
         return value / 70;
-
     }
-    public static float rubleToDollar(float value){
+    public static float rubleToDollar(float value) {
         return value / 60;
     }
-    public static void main(String [] args) {
-        float euro = Conventer.rubleToEuro(140);
-        float dollar = Conventer.rubleToDollar(140);
-        System.out.println("140 rubles are " +  euro + " euro");
-        System.out.println("140 rubles are " + dollar + " dollar");
-
+    public static void main(String[] args) {
+        int inDollar = 120;
+        int inEuro = 140;
+        float outDollar = Conventer.rubleToDollar(inDollar);
+        float outEeuro = Conventer.rubleToEuro(inEuro);
+        boolean passedD = outDollar == 2;
+        boolean passedE = outEeuro == 2;
+        System.out.println("140 rubles are " +  outEeuro + " euro. " + "test result: " + passedE);
+        System.out.println("120 rubles are " + outDollar + " dollar. " + "test result: " + passedD);
     }
 
 }

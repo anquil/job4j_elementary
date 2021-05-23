f92397e934c2354051b954b9d3c15f18207124c0
Anquil <7529054@mail.ru>
2021-05-23 19:06:25 +0300

    9/1 фиксация изменений на github [#478616]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int outEuro = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, outEuro);
+    }
+
+    @Test
+    public void whenConvert180rubleToDollar() {
+        int in = 180;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}

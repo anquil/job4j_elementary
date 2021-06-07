725e4058432b9129b0952857e527f071b5667689
Anquil <7529054@mail.ru>
2021-06-08 02:59:44 +0300

    9/1 фиксация изменений на github [#235631]

--- src/ru/job4j/ArgMethod.java
+++ src/ru/job4j/ArgMethod.java
@@ -5,10 +5,10 @@ public class ArgMethod {
 
         System.out.println("Hello, " + name);
     }
-    public static void age(int age){
+    public static void age(int age) {
         System.out.println("Age = " + age);
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
          ArgMethod.hello(name);

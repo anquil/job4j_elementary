414fe8f57b100bd9f21e422df76bdaac871fd499
Anquil <7529054@mail.ru>
2021-06-08 00:12:12 +0300

    9/1 фиксация изменений на github [#159458]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        short in = 187;
+        double expected = 85.5;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void womanWeight() {
+        short in = 187;
+        double expected = 75.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}

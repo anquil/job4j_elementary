70207129965bbff0c178ee2ae89d281172b92b77
Anquil <7529054@mail.ru>
2021-06-15 05:40:56 +0300

    9/1 фиксация изменений на github [#159]

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void maxNumber() {
+        int left = 3;
+        int right = 5;
+        int expected = 5;
+        int rsl = Max.max(left, right);
+        Assert.assertEquals(expected, rsl);
+    }
+}
